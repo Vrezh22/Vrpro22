c1049b10a964b51113791821a07a1449cf7df1a3
Vrezh22 <vrezh22hovhannisyan@mail.ru>
2021-01-31 23:54:00 +0400

    vrProjecto

--- src/components/ToDO/index.jsx
+++ src/components/ToDO/index.jsx
@@ -1,12 +1,26 @@
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col,Button } from 'react-bootstrap';
 import React from 'react';
+import { connect } from 'react-redux';
 import TodoForm from '../TodoForm';
 import Task from '../Task';
-import { connect } from 'react-redux';
+import EditTaskModal from '../Confirm';
 
 class Todo extends React.Component {
     state = {
-        inputValue: ''
+        inputValue: '',
+        editTask: null,
+        removeTasks: new Set()
+    }
+    toggleSetAnyTasks = (_id) => {
+        const removeTasks = new Set(this.state.removeTasks);
+        if (removeTasks.has(_id))
+            removeTasks.delete(_id);
+        else
+            removeTasks.add(_id);
+
+        this.setState({
+            removeTasks
+        })
     }
     handleOnChange = ({ target }) => {
         this.setState({
@@ -17,58 +31,92 @@ class Todo extends React.Component {
         if (!this.state.inputValue)
             return;
         this.props.addPost(this.state.inputValue);
+
         this.setState({
             inputValue: ''
         })
     }
     handleDeleteForm = (_id) => {
         return this.props.deletePost(_id);
+
+    }
+    clearEditTask = () => {
+        this.setState({
+            editTask: null
+        });
+    }
+    setEditTask = (editTask) => {
+        this.setState(prevState => ({
+            ...prevState,
+            editTask
+        }))
     }
 
-
     render() {
-        const { tasks } = this.props;
-        const { inputValue } = this.state;
+        const { tasks, editOneTask } = this.props;
+        const { inputValue, editTask  ,removeTasks} = this.state;
         const tasksJSX = tasks.map(task => {
-
+            const colInlineStyle = {
+                border: '1px solid black',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                padding: '15px'
+            }
             return (
-                <Col key={task._id} xs={12} sm={6} md={4} style={{ border: '1px solid black' }}>
-                    <Task task={task} handleDeleteForm={this.handleDeleteForm} />
+                <Col key={task._id} xs={12} sm={6} md={4} style={colInlineStyle} >
+                    <Task
+                        task={task}
+                        handleDeleteForm={this.handleDeleteForm}
+                        setEditTask={this.setEditTask}
+                        toggleSetAnyTasks={this.toggleSetAnyTasks}
+                        disabled={!!removeTasks.size}
+                    />
                 </Col>
             )
         })
 
         return (
-            <div>
+            <>
                 <Container>
                     <Row className="justify-content-center">
                         <TodoForm
                             inputValue={inputValue}
                             handleOnChange={this.handleOnChange}
                             handleSubmitForm={this.handleSubmitForm}
+                            disabled={!!removeTasks.size}
                         />
                     </Row>
                     <Row>
                         {tasksJSX}
                     </Row>
+                    <Row className="mt-5">
+                         <Button variant="danger" disabled={!removeTasks.size}>Delete All</Button> 
+                    </Row>
                 </Container>
-
-            </div>
+                {
+                    editTask && <EditTaskModal
+                        onHide={this.clearEditTask}
+                        data={editTask}
+                        editOneTask={editOneTask}
+                    />
+                }
+            </>
         )
     }
 }
-
 const mapStateToProps = (state) => {
     return {
         tasks: state.todoState.tasks
     }
 }
 const mapDispacthToProps = (dispatch) => {
-    return{
-        addPost: (text)=> dispatch({type:'ADDPOST' , text:text}),
-        deletePost:(_id)=> dispatch({type:'DELETEPOST', _id:_id})
+    return {
+        addPost: (text) => dispatch({ type: 'ADDTask', text: text }),
+        deletePost: (_id) => dispatch({ type: 'DELETETask', _id: _id }),
+        editOneTask: (task) => dispatch({ type: 'EDIT_TASK', task })
     }
 }
-const TodoContainer = connect(mapStateToProps,mapDispacthToProps)(Todo);
-
+const TodoContainer = connect(mapStateToProps, mapDispacthToProps)(Todo);
 export default TodoContainer;
--- src/components/TodoForm/index.jsx
+++ src/components/TodoForm/index.jsx
@@ -1,18 +1,20 @@
 import styles from './todoForm.module.css';
 
-const TodoForm = ({inputValue , handleOnChange , handleSubmitForm})=>{
-    return(
+const TodoForm = ({ inputValue, handleOnChange, handleSubmitForm, disabled }) => {
+    return (
         <div className={styles.todoForm}>
             <input
-            type='text'
-            placeholder='Գրիր նոր նյութ'
-            value={inputValue}
-            onChange={handleOnChange}
+                type='text'
+                placeholder='Գրիր նոր նյութ'
+                value={inputValue}
+                onChange={handleOnChange}
+                disabled={disabled}
             />
             <input
-             type='button'
-             value='Add'
-             onClick={handleSubmitForm}
+                type='button'
+                value='Add'
+                onClick={handleSubmitForm}
+                disabled={disabled}
             />
         </div>
     )
